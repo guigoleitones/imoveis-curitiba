8cc010c0ab702f3e2dc5e238fdc1844516da95ce
guigoleitones <guigoleitones@gmail.com>
2026-03-18 23:45:59 -0300

    app.py

--- app.py
+++ app.py
@@ -6,8 +6,28 @@ st.set_page_config(page_title="Imóveis Curitiba", layout="wide")
 st.title("🏠 Imóveis em Curitiba")
 st.write("Encontre os melhores imóveis para alugar em Curitiba")
 
-# Carregar CSV
-df = pd.read_csv("imoveis.csv")
+# Dados direto no código
+dados = {
+    "Título": [
+        "Apartamento 2Q - Centro",
+        "Casa 3Q - Batel",
+        "Studio - Água Verde",
+        "Terreno - Portão"
+    ],
+    "Preço": ["R$ 1.500", "R$ 2.500", "R$ 800", "R$ 5.000"],
+    "Localização": ["Centro", "Batel", "Água Verde", "Portão"],
+    "Descrição": ["Bem localizado", "Com garagem", "Mobiliado", "500m²"],
+    "Link": [
+        "https://www.olx.com.br/imoveis/apartamento-2-quartos-centro-curitiba",
+        "https://www.zapimoveis.com.br/aluguel/apartamento-3-quartos-batel-curitiba-pr",
+        "https://www.imoveiswebcuritiba.com.br/studio-agua-verde",
+        "https://www.vilarealcuritiba.com.br/terreno-portao"
+    ],
+    "Site": ["OLX", "Zap", "Imóveis Web", "Vila Real"],
+    "Data": ["19/03/2026", "19/03/2026", "19/03/2026", "19/03/2026"]
+}
+
+df = pd.DataFrame(dados)
 
 # Filtros
 col1, col2 = st.columns(2)
